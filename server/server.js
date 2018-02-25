812dd0727f57618a460be69ba87463ea2cea0baa
Joel Guizar <joelguizar@gmail.com>
2018-02-25 15:54:29 -0500

    secret keys/apis

--- server/server.js
+++ server/server.js
@@ -5,11 +5,22 @@ const PORT = process.env.PORT || 3000;
 const passport = require('passport');
 //passport-google-oauth20 comes with a lot, but we only want the .Strategy property.
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
 
-clientID= '150881180895-2a30p8o71k73evvvum9bguurpnpbj6rc.apps.googleusercontent.com'
-clientSecret='5duP7mvwzLpGvuIjhJMW_WBK'
+
 //uses a new strategy/protcol instance inside the app
-passport.use(new GoogleStrategy())
+//but we need to give configuration to these strategy
+//lets pass in the keys/secrets here from keys.js
+
+//the third option we pass into the strategy, is which route we want Google to redirect
+//the user to once it finishes the authentication -- usually we direct them to a route
+//where we can now use the token from google to use permissions that require auth
+passport.use(
+  new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret
+  })
+)
 
 app.listen(PORT, ()=>{
   console.log(`Listening on PORT ${PORT}`);
