783cd4520896b86849d5348cc62470f15db0e257
Joel Guizar <joelguizar@gmail.com>
2018-02-25 20:42:25 -0500

    google auth route bug fix

--- server/server.js
+++ server/server.js
@@ -12,15 +12,34 @@ const keys = require('../config/keys');
 //but we need to give configuration to these strategy
 //lets pass in the keys/secrets here from keys.js
 
-//the third option we pass into the strategy, is which route we want Google to redirect
+//the third option 'callbackURL' we pass into the strategy, is which route we want Google to redirect
 //the user to once it finishes the authentication -- usually we direct them to a route
 //where we can now use the token from google to use permissions that require auth
+//once we specify the callbackURL, we must make the corresponding route in express/node
 passport.use(
-  new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret
-  })
+  new GoogleStrategy(
+    //configuration as 1st argument
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      //callbackURL must match the authorized redirect URIs on google
+      callbackURL: '/auth/google/callback'
+    },
+    //callback as 2nd argument
+    (accessToken)=>{
+      console.log(accessToken);
+    }
+  )
 )
+
+//instead of just an req/res arrow function, use passport function
+//internally the 'GoogleStrategy' has the identifier as the string google
+//2nd argument is an obect -- 'scope' key being which permissions we want from google
+  //a full list of permissions is on their page
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}))
 
 app.listen(PORT, ()=>{
   console.log(`Listening on PORT ${PORT}`);
